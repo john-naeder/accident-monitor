3cb1995c0e030b3565e50f9299d6f75902c16a38
John Naeder <johnnaeder6537@gmail.com>
2025-04-10 18:17:16 +0700

    Fix (AccidentConfiguration): add coordinate constraint

--- accident-monitor/src/Infrastructure/Migrations/20250410102939_AddAccidentCoordinateConstraint.Designer.cs
+++ accident-monitor/src/Infrastructure/Migrations/20250410102939_AddAccidentCoordinateConstraint.Designer.cs
@@ -4,6 +4,7 @@ using AccidentMonitor.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AccidentMonitor.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250410102939_AddAccidentCoordinateConstraint")]
+    partial class AddAccidentCoordinateConstraint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ accident-monitor/src/Infrastructure/Migrations/20250410102939_AddAccidentCoordinateConstraint.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccidentMonitor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccidentCoordinateConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Coordinate_Latitude_Range",
+                table: "PolygonCoordinates");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Coordinate_Longitude_Range",
+                table: "PolygonCoordinates");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Coordinate_Latitude_Range1",
+                table: "PolygonCoordinates",
+                sql: "[Latitude] >= -90 AND [Latitude] <= 90");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Coordinate_Longitude_Range1",
+                table: "PolygonCoordinates",
+                sql: "[Longitude] >= -180 AND [Longitude] <= 180");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Coordinate_Latitude_Range",
+                table: "Accidents",
+                sql: "[Latitude] >= -90 AND [Latitude] <= 90");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Coordinate_Longitude_Range",
+                table: "Accidents",
+                sql: "[Longitude] >= -180 AND [Longitude] <= 180");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Coordinate_Latitude_Range1",
+                table: "PolygonCoordinates");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Coordinate_Longitude_Range1",
+                table: "PolygonCoordinates");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Coordinate_Latitude_Range",
+                table: "Accidents");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Coordinate_Longitude_Range",
+                table: "Accidents");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Coordinate_Latitude_Range",
+                table: "PolygonCoordinates",
+                sql: "[Latitude] >= -90 AND [Latitude] <= 90");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Coordinate_Longitude_Range",
+                table: "PolygonCoordinates",
+                sql: "[Longitude] >= -180 AND [Longitude] <= 180");
+        }
+    }
+}
