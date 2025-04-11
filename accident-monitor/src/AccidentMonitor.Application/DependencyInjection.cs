cabad16fed3deccf288efe76cfe4aaa150d5a3ca
John Naeder <johnnaeder6537@gmail.com>
2025-04-11 16:30:55 +0700

    Refactor (accident-monitor *.prj+.sln): add AccidentMonitor prefix for project name

---
+++ accident-monitor/src/AccidentMonitor.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using AccidentMonitor.Application.Common.Behaviours;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AccidentMonitor.Application;
+public static class DependencyInjection
+{
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        builder.Services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
+        });
+    }
+}
--- accident-monitor/src/Infrastructure/ORS/ORSConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AccidentMonitor.Infrastructure.ORS;
-public class ORSConfiguration
-{
-    public required Uri Uri { get; set; }
-    public required int Port { get; set; }
-    public required string BasePath { get; set; }
-    public string ApiKey { get; set; } = string.Empty;
-}
